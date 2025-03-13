0a45aea9c90325b19ed6959dcfb2ecf8cfab2552
Ivan <ivan.karabankov@gmail.com>
2025-03-13 10:41:05 +0300

    3. Fetched names by clicking on a letter in nav

--- index.php
+++ index.php
@@ -1,10 +1,32 @@
 <?php
 
 require __DIR__ . '/inc/all.inc.php';
-?>
 
+$char = (string) ($_GET['char'] ?? '');
+if (strlen($char) > 1) {
+  $char = $char[0];
+}
+$char = strtoupper($char);
+
+$stmt = $pdo->prepare('SELECT DISTINCT `name` FROM `names` WHERE `name` LIKE :letter ORDER BY `names`.`name` ASC');
+$stmt->bindValue(':letter', "{$char}%");
+$stmt->execute();
+$results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+$names = [];
+foreach ($results as $result) {
+  $names[] = $result['name'];
+}
+?>
 <?php require __DIR__ . '/views/header.php'; ?>
 
-
+<ul>
+  <?php foreach ($names as $name): ?>
+    <li>
+      <a href="name.php?<?php echo http_build_query(['name' => $name]); ?>">
+        <?php echo e($name); ?>
+      </a>
+    </li>
+  <?php endforeach; ?>
+</ul>
 
 <?php require __DIR__ . '/views/footer.php'; ?>
