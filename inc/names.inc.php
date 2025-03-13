139da6cf0fc9bd2a237c17b360ba678b5d920273
Ivan <ivan.karabankov@gmail.com>
2025-03-13 12:17:32 +0300

    5. Created names.inc.php file and moved fetch_names_by_initial there to refactor the code

---
+++ inc/names.inc.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+function fetch_names_by_initial(string $char): array
+{
+  global $pdo;
+  $names = [];
+
+  $stmt = $pdo->prepare('SELECT DISTINCT `name` FROM `names` WHERE `name` LIKE :letter ORDER BY `names`.`name` ASC');
+  $stmt->bindValue(':letter', "{$char}%");
+  $stmt->execute();
+  $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+  foreach ($results as $result) {
+    $names[] = $result['name'];
+  }
+
+  return $names;
+}
+
--- index.php
+++ index.php
@@ -8,22 +8,6 @@ if (strlen($char) > 1) {
 }
 $char = strtoupper($char);
 
-function fetch_names_by_initial($char)
-{
-  global $pdo;
-  $names = [];
-
-  $stmt = $pdo->prepare('SELECT DISTINCT `name` FROM `names` WHERE `name` LIKE :letter ORDER BY `names`.`name` ASC');
-  $stmt->bindValue(':letter', "{$char}%");
-  $stmt->execute();
-  $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
-
-  foreach ($results as $result) {
-    $names[] = $result['name'];
-  }
-
-  return $names;
-}
 
 $names = fetch_names_by_initial($char);
 
